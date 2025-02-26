660067eac1c0e1173fa4f022cf0a543c80aa3ac0
Bobby <karma0@gmail.com>
2025-02-25 22:07:24 -0700

    First pass

---
+++ main.py
@@ -0,0 +1,11 @@
+# main.py
+import time, machine
+
+# Use the same LED (GPIO2)
+led = machine.Pin(2, machine.Pin.OUT)
+
+while True:
+    led.on()
+    time.sleep(0.5)
+    led.off()
+    time.sleep(0.5)
